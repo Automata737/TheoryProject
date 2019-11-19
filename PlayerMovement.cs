c21392d9e6496dcc8368a838dcf0e77fc9e24723
Automata737 <57932573+Automata737@users.noreply.github.com>
2019-11-19 00:17:46 -0500

    C# code for player movement
    The following code is used to change the direction in which the player moves along with the force at which it moves.

---
+++ PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // This is the reference to the Rigidbody componenet called rb
+    public Rigidbody rb;
+
+    public float forwardForce = 800f;
+    public float sidewaysForce = 500f;
+    // We marked this as FixedUpdate because we are using it to mess with physics.
+    void FixedUpdate()
+    {
+        // Add a forward force.
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if(Input.GetKey("d")) // Move right
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0,0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a")) // Move left
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
