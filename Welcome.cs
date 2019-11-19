ba04100b491305f31493e916fc55826f59147d5a
Automata737 <57932573+Automata737@users.noreply.github.com>
2019-11-19 00:24:40 -0500

    C# code for welcome and next
    This code is used to navigate from screen to screen with the use of buttons.

---
+++ Welcome.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine.SceneManagement;
+
+using UnityEngine;
+
+public class Welcome : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
